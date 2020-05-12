0c4beac71c86f233c7445798298dfae3bb1b55f2
stiria06 <bonny06@naver.com>
2020-05-13 03:14:45 +0900

    Add boj_14500

--- BOJ/BOJ/boj_14500.cpp
+++ BOJ/BOJ/boj_14500.cpp
@@ -4,7 +4,7 @@
 int N, M;
 int arr[500][500], visited[500][500];
 int dx[] = { 1, 0, -1, 0 }, dy[] = { 0, 1, 0, -1 };
-int ans = 0;
+int ans = 0, i, j;
 
 void dfs(int tx, int ty, int cnt, int sum) {
 	if (cnt == 4) {
@@ -13,7 +13,7 @@ void dfs(int tx, int ty, int cnt, int sum) {
 	}
 	for (int k = 0; k < 4; k++) {
 		int x = tx + dx[k], y = ty + dy[k];
-		if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y]) continue;
+		if (x < 0 || y < 0 || x >= N || y >= M || visited[x][y] || x < i) continue;
 		visited[x][y] = 1;
 		dfs(x, y, cnt + 1, sum + arr[x][y]);
 		visited[x][y] = 0;
@@ -24,8 +24,8 @@ int main() {
 	scanf("%d %d", &N, &M);
 	for (int n = 0; n < N; n++) for (int m = 0; m < M; m++) scanf("%d", &arr[n][m]);
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
 			visited[i][j] = 1;
 			dfs(i, j, 1, arr[i][j]);
 			visited[i][j] = 0;
